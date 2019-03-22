46437c2cd35b0153d9a093edfb3cf537cedb67af
Atif Mahmud <atifmahmud95@gmail.com>
2019-03-22 15:47:36 -0700

    Added click listeners

--- app/src/main/java/com/example/atifm/breathingexercises/ExercisesActivity.java
+++ app/src/main/java/com/example/atifm/breathingexercises/ExercisesActivity.java
@@ -5,14 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ExercisesActivity extends AppCompatActivity {
+public class ExercisesActivity extends AppCompatActivity implements MenuItem.OnMenuItemClickListener, PopupMenu.OnMenuItemClickListener {
 
     private TextView mTextMessage;
 
@@ -65,15 +64,30 @@ public class ExercisesActivity extends AppCompatActivity {
     }
 
     // method for debugging
-    private void showTestToast() {
-        Toast.makeText(getApplicationContext(),"HelloHello",Toast.LENGTH_SHORT).show();
+    private void showTestToast(String string) {
+        Toast.makeText(getApplicationContext(),string + " Hello ", Toast.LENGTH_SHORT).show();
     }
 
 
     private void createExercisePopupMenu(View v) {
         PopupMenu changeExerciseMenu = new PopupMenu(this, v);
+        changeExerciseMenu.setOnMenuItemClickListener(this);
         changeExerciseMenu.inflate(R.menu.exercise_menu);
         changeExerciseMenu.show();
     }
 
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        showTestToast((String) menuItem.getTitle());
+        return false;
+    }
+
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
+
